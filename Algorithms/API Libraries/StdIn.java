899ea2353794b6713bd3fe623bbb126093f7cab7
William Yi <williamyi96@gmail.com>
2017-03-04 21:30:34 +0800

    Update StdIn.java

--- Algorithms/API Libraries/StdIn.java
+++ Algorithms/API Libraries/StdIn.java
@@ -1,1 +1,15 @@
-
+//API for the used library of static methods for standard input
+public class StdIn {
+    static boolean isEmpty();   //true if no more values, false otherwise
+    static int readInt();   //read a value of type int
+    static double readDouble(); //read a value of type double
+    static float readFloat();   //read a value of type float
+    static long readLong(); //read a value of type long
+    static boolean readBoolean();   //read a value of type boolean
+    static char readChar(); //read a value of type char
+    static byte readByte(); //read a value of type byte
+    static String readString(); //read a value of type String
+    static boolean hasNextLine();   //is there another line in the input stream?
+    static String readLine();   //read the rest of the line
+    static String readAll();    //read the rest of the input stream
+}
