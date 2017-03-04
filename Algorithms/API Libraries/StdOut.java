80bf9b82b18288e3daffa687c8a26fe85f272683
William Yi <williamyi96@gmail.com>
2017-03-04 21:28:48 +0800

    Create StdOut.java

---
+++ Algorithms/API Libraries/StdOut.java
@@ -0,0 +1,7 @@
+//API for the used library of static methods for standard output
+public class StdOut {
+    static void print(String s);    //print s
+    static void println(String s);  //print s, followed by newline
+    static void println();     //print a new line
+    //more formation can be added if neccessary
+}
